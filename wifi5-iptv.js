205bb5d68d00aca769b1c5dd6ea9e5d9a49c6bea
SSGxFILES <ssg.pisowifi.me@gmail.com>
2025-04-03 21:37:32 +0800

    Add files via upload

---
+++ wifi5-iptv.js
@@ -0,0 +1,110 @@
+// HELLO FRIEND!!! Nothing can change here. Dont need to open or inspect this file. Be a good PERSON ^_^. 
+// Exodus 20:15 “You shall not steal“.
+document.head.insertAdjacentHTML("beforeend", `<style id="NoNav">#sidebar, .navbar-toggler, .fixed-top{display:none!important}.page-body-wrapper{width:100%!important;transition:none}.main-panel{padding-top:0!important}</style>`);
+document.head.insertAdjacentHTML("beforeend",`<style id="blank_me">html{background: #000;}.container-scroller,#chati{display: none;}#spinner{color: #000}</style>`);
+document.head.insertAdjacentHTML("beforeend",`<style id="addCss">.container-scroller{animation: fadeIn .8s;}.mdi-theme-light-dark{display: none;}.spin i{left: calc(50% - 30px)!important;}</style>`);
+document.head.insertAdjacentHTML("beforeend", `<style id="prefix"></style>`);
+_0001();
+(function($){
+    $(document).ready(function(){  
+        _0011();
+    });
+})(jQuery); 
+function _0001(){
+    fetch('/main/script/main.js')
+    .then(response => response.text())
+    .then(txt => eval(txt))
+    .catch((response) => {
+        $(atob("I3ByZWZpeA==")).text(atob("Tm9TZXJpYWw="));
+    })
+}
+function _0011(){
+    $("#spinner i").attr("class","fa fa-cog fa-spin fa-3x fa-fw");   
+    waitForElm(atob("I3RoZW1lLXNldA==")).then((elm)=>{    
+        if ($(atob("I3RoZW1lLXNldA==")).length) {
+            $("#blank_me").remove();
+            var myVar = $('#theme-set').text().replace(/;/g, ":").split(":");
+            var _ssg0 = $.trim(myVar[0]);
+            if (_ssg0 == "SSGx000 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx000.js"></script>'); 
+            }else if (_ssg0 == "SSGx001 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx001.js"></script>'); 
+            }else if (_ssg0 == "SSGx002 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx002.js"></script>');
+            }else if (_ssg0 == "SSGx003 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx003.js"></script>'); 
+            }else if (_ssg0 == "SSGx004 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx004.js"></script>'); 
+            }else if (_ssg0 == "SSGx005 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx005.js"></script>');    
+            }else if (_ssg0 == "SSGx006 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx006.js"></script>'); 
+            }else if (_ssg0 == "SSGx007 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx007.js"></script>'); 
+            }else if (_ssg0 == "SSGx008 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx008.js"></script>'); 
+            }else if (_ssg0 == "SSGx009 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx009.js"></script>'); 
+            }else if (_ssg0 == "SSGx010 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx010.js"></script>'); 
+            }else if (_ssg0 == "SSGx011 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx011.js"></script>'); 
+            }else if (_ssg0 == "SSGx012 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx012.js"></script>');
+            }else if (_ssg0 == "SSGx013 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx013.js"></script>'); 
+            }else if (_ssg0 == "SSGx014 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx014.js"></script>'); 
+            }else if (_ssg0 == "SSGx015 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx015.js"></script>');    
+            }else if (_ssg0 == "SSGx016 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx016.js"></script>'); 
+            }else if (_ssg0 == "SSGx017 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx017.js"></script>'); 
+            }else if (_ssg0 == "SSGx018 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx018.js"></script>');
+            }else if (_ssg0 == "SSGx019 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx019.js"></script>'); 
+            }else if (_ssg0 == "SSGx020 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx020.js"></script>'); 
+            }else if (_ssg0 == "SSGx021 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx021.js"></script>'); 
+            }else if (_ssg0 == "SSGx022 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx022.js"></script>'); 
+            }else if (_ssg0 == "SSGx023 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx023.js"></script>'); 
+            }else if (_ssg0 == "SSGx024 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx024.js"></script>'); 
+            }else if (_ssg0 == "SSGx025 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx025.js"></script>'); 
+            }else if (_ssg0 == "SSGx026 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx026.js"></script>'); 
+            }else if (_ssg0 == "SSGx027 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx027.js"></script>'); 
+            }else if (_ssg0 == "SSGx028 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx028.js"></script>'); 
+            }else if (_ssg0 == "SSGx029 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx029.js"></script>'); 
+            }else if (_ssg0 == "SSGx030 THEME ID"){
+                $('head script:first').before('<script type="text/javascript" src="/html/SSGx030.js"></script>'); 
+            }            
+        }
+    });
+}
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
